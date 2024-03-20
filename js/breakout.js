780bf68df01550b33b85250ee15d5e3ab7bc096a
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-03-20 16:52:55 +0000

    Wed, Mar 20, 2024, 9:52 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,8 +4,16 @@ closesBtn = document.getElemebtById('close-btn')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
-ctx.fillStyle = "green"
-ctx.fillRect(10, 10, 150, 100);
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height /2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy; -4;
+}
+
+
 
 
 rulesBtn.addEventListener('click', () => {
