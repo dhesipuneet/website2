be7cdfaaf34028611abdfba32b9938ff3aaba89f
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-03-20 17:09:14 +0000

    Wed, Mar 20, 2024, 10:09 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -46,8 +46,8 @@ function drawPaddle() {
 
 
 function drawScore() {
-
-    ctx.fillText(`Score: ${score}, canvas.width-100, 30)
+    ctx.font ='20px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
 }
 
 function draw(){
