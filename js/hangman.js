91d9e12a3b6e36f65857e4a917dfb8b8840def7c
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-04-22 16:50:23 +0000

    Mon, Apr 22, 2024, 9:50 AM -07:00

---
+++ js/hangman.js
@@ -0,0 +1,12 @@
+const wordEL = document.getElementById('word')
+wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['application', 'programming', 'interface', 'wizard']
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[sele]
