0cae8d4f628c2e0730a993256e9b86580de9b9fa
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-04-22 16:53:12 +0000

    Mon, Apr 22, 2024, 9:53 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -9,4 +9,18 @@ const figureParts = document.querySelectorAll('.figure-part')
 const word = ['application', 'programming', 'interface', 'wizard']
 
 let selectedIndex = Math.floor(word.length * Math.random())
-let selectedWord = word[sele]
+let selectedWord = word[selectedIndex]
+
+const correctLetters = []
+const wrongLetters = []
+
+function displayWord() {
+    wordEl.innerHTML = `
+    ${selectedWord}
+
+
+    `
+
+}
+
+displayWord()
