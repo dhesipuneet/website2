8f8b545c2019a0291b5dd8391e6f12a4ce4848ec
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-05-22 03:32:07 +0000

    Tue, May 21, 2024, 8:32 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,88 +1,54 @@
-const screens = document.querySelectorAll('.screen')
-const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
-const game_container = document.getElementById('game-container')
-const start_btn = document.getElementById('start-btn')
-const timeEl = document.getElementById('time')
-const scoreEl = document.getElementById('score')
-const message = document.getElementById('message')
-let seconds = 0
-let score = 0
-let selected_insect = {}
-const win = document.getElementById('W')
-const loss = document.getElementById('L')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catch The Insect</title>
+    <link rel="stylesheet" href="css/insect.css">
+</head>
+<body>
+    <div class = "screen">
+        <h1>Catch The Insect</h1>
+        <button class = "btn" id = "start-btn">Play Game</button>
+    </div>
 
-start_btn.addEventListener('click',() => {
-    screens[0].classList.add('up')
-})
-
-choose_insect_btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const img = btn.querySelector('img')
-        const alt = img.getAttribute('alt')
-        const src = img.getAttribute('src')
-        screens[1].classList.add('up')
-        selected_insect = {src,alt}
-        setTimeout(createInsect, 1000)
-        startGame()
-    })
-})
-
-function createInsect() {
-    const insect = document.createElement('div')
-    insect.classList.add('insect')
-    const {x,y} = getRandomLocation()
-    insect.style.top = `${y}px`
-    insect.style.left = `${x}px`
-    insect.innerHTML = `<img src="${selected_insect.src}" alt=${selected_insect.alt} style = "transform: rotate(${Math.random() * 360}deg)" />`
-
-    insect.addEventListener('click', catchInsect)
-
-    game_container.appendChild(insect)
-}
-
-function catchInsect() {
-    increaseScore()
-    console.log(this)
-    this.classList.add('caught')
-    setTimeout(() => this.remove(), 1000)
-    addInsects()
-}
-
-function addInsects() {
-    setTimeout(createInsect, 1000)
-    setTimeout(createInsect, 1500)
-}
-
-function startGame() {
-    setInterval(increaseTime, 1000)
-}
-
-function increaseTime() {
-    let m = Math.floor(seconds / 60)
-    let s = seconds % 60
-    if (m < 10) {
-        m = `0${m}`
-    }
-    if (s < 10) {
-        s = `0${s}`
-    }
-    timeEl.innerHTML = `Time: ${m}:${s}`
-    seconds++
-
-}
-
-function increaseScore() {
-    score++
-    if (score > 19) {
-        message.classList.add('visible')
-    }
-    scoreEl.innerHTML = `Score: ${score}`
-}
-
-function getRandomLocation() {
-    const width = window.innerWidth
-    const height = window.innerHeight
-    const x = Math.random() * (width - 200) + 100
-    const y = Math.random() * (height - 200) + 100
-    return {x,y}
-}
+    <div class = "screen">
+        <h1>What is your favorite insect?</h1>
+        <ul class = "insects-list">
+            <li>
+                <button class = "choose-insect-btn">
+                    <p>Fly</p>
+                    <img src = "images/fly.png" alt = "fly">
+                </button>
+            </li><li>
+                <button class = "choose-insect-btn">
+                    <p>mosquito</p>
+                    <img src = "images/mosquito.png" alt = "mosquito">
+                </button>
+            </li>
+            <li>
+                <button class = "choose-insect-btn">
+                    <p>roach</p>
+                    <img src = "images/roach.png" alt = "roach">
+                </button>
+            </li>
+            <li>
+                <button class = "choose-insect-btn">
+                    <p>spider</p>
+                    <img src = "images/spider.png" alt = "spider">
+                </button>
+            </li>
+        </ul>
+    </div>
+    <div class="screen game-container" id="game-container">
+        <h3 class="time" id="time">
+            time: 00:00</h3>
+            <h3 id="score" class="score">Score: 0</h3>
+        <h5 id="message" class="message">
+            Are you annoyed yet? <br>
+            You are playing an impossible game!!
+        </h5>
+    </div>
+</body>
+<script src= "js/insect.js"></script>
+</html>
